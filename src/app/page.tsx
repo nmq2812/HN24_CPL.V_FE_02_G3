b25828af6c35a0b9e941c33550940c4460e5bd12
21020383-NGUYENMINHQUAN <nguyenminhquan341@gmail.com>
2024-08-22 21:54:45 +0700

    update setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use server";
 import "./page.module.css";
 import TagList from "@/components/tagList";
 import "antd/dist/reset.css";
@@ -5,32 +6,34 @@ import { Layout, Row, Col } from "antd";
 import GlobalFeed from "@/components/Feed/GlobalFeed";
 import { Content } from "antd/es/layout/layout";
 
-export default function Home() {
-  return (
-    <Layout style={{ minHeight: "100vh", maxWidth: "80%", margin: "0 auto" }}>
-      <Content style={{ padding: "0 24px", marginTop: "16px" }}>
-        <Row gutter={24} align="top">
-          {/* Bên trái: Danh sách bài viết */}
-          <Col xs={24} md={18} lg={16} style={{ padding: "0" }}>
-            <GlobalFeed />
-          </Col>
+export default async function Home() {
+    return (
+        <Layout
+            style={{ minHeight: "100vh", maxWidth: "80%", margin: "0 auto" }}
+        >
+            <Content style={{ padding: "0 24px", marginTop: "16px" }}>
+                <Row gutter={24} align="top">
+                    {/* Bên trái: Danh sách bài viết */}
+                    <Col xs={24} md={18} lg={16} style={{ padding: "0" }}>
+                        <GlobalFeed />
+                    </Col>
 
-          {/* Bên phải: Danh sách thẻ tag */}
-          <Col
-            xs={24}
-            md={6}
-            lg={8}
-            style={{
-              padding: "0",
-              // position: "-webkit-sticky",
-              position: "sticky",
-              top: "0",
-            }}
-          >
-            <TagList />
-          </Col>
-        </Row>
-      </Content>
-    </Layout>
-  );
+                    {/* Bên phải: Danh sách thẻ tag */}
+                    <Col
+                        xs={24}
+                        md={6}
+                        lg={8}
+                        style={{
+                            padding: "0",
+                            // position: "-webkit-sticky",
+                            position: "sticky",
+                            top: "0",
+                        }}
+                    >
+                        <TagList />
+                    </Col>
+                </Row>
+            </Content>
+        </Layout>
+    );
 }
