1d2316da6dd54a015921dcd0f6eefd145a46b2a2
21020383-NGUYENMINHQUAN <nguyenminhquan341@gmail.com>
2024-08-23 17:33:39 +0700

    completed search tag

--- src/actions/handleArticle.ts
+++ src/actions/handleArticle.ts
@@ -1,69 +1,69 @@
 const BASE_URL = "https://node-express-conduit.appspot.com/api";
 
 export const getArticles = async ({
-  limit,
-  page,
-  tag,
-  author,
-  favorited,
+    limit,
+    page,
+    tag,
+    author,
+    favorited,
 }: GlobalArticleParams) => {
-  try {
-    const queryParams = new URLSearchParams({
-      limit: limit.toString(),
-      page: page.toString(),
-      ...(author && { author }),
-      ...(favorited && { favorited }),
-      ...(tag && { tag }),
-    });
-    const response = await fetch(`${BASE_URL}/articles?${queryParams}`);
-    const res = await response.json();
-    if (response.status === 200) {
-      return { success: true, data: res.articles };
-    } else {
-      if (response.status === 401 || response.status === 422) {
-        return { success: false, message: res };
-      } else {
+    try {
+        const queryParams = new URLSearchParams({
+            limit: limit.toString(),
+            page: page.toString(),
+            ...(author && { author }),
+            ...(favorited && { favorited }),
+            ...(tag && { tag }),
+        });
+        const response = await fetch(`${BASE_URL}/articles?${queryParams}`);
+        const res = await response.json();
+        if (response.status === 200) {
+            return { success: true, data: res.articles };
+        } else {
+            if (response.status === 401 || response.status === 422) {
+                return { success: false, message: res };
+            } else {
+                return {
+                    success: false,
+                    message: { errors: { message: "Unexpected error" } },
+                };
+            }
+        }
+    } catch (error) {
         return {
-          success: false,
-          message: { errors: { message: "Unexpected error" } },
+            success: false,
+            message: { errors: { message: "An error occurred" } },
         };
-      }
     }
-  } catch (error) {
-    return {
-      success: false,
-      message: { errors: { message: "An error occurred" } },
-    };
-  }
 };
 
 export const getClickedArticle = async (slug: string, token?: string) => {
-  try {
-    const response = await fetch(`${BASE_URL}/articles/${slug}`, {
-      method: "GET",
-      headers: { Authorization: token ? `Bearer ${token}` : "" },
-    });
+    try {
+        const response = await fetch(`${BASE_URL}/articles/${slug}`, {
+            method: "GET",
+            headers: { Authorization: token ? `Bearer ${token}` : "" },
+        });
 
-    const res = await response.json();
-    return res;
-  } catch {
-    const result: Article = {
-      slug: "error",
-      title: "error",
-      description: "error",
-      body: "error",
-      tagList: [],
-      createdAt: "",
-      updatedAt: "",
-      favorited: false,
-      favoritesCount: 0,
-      author: {
-        username: "error",
-        bio: "error",
-        image: "error",
-        following: false,
-      },
-    };
-    return result;
-  }
+        const res = await response.json();
+        return res;
+    } catch {
+        const result: Article = {
+            slug: "error",
+            title: "error",
+            description: "error",
+            body: "error",
+            tagList: [],
+            createdAt: "",
+            updatedAt: "",
+            favorited: false,
+            favoritesCount: 0,
+            author: {
+                username: "error",
+                bio: "error",
+                image: "error",
+                following: false,
+            },
+        };
+        return result;
+    }
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,103 @@
-"use server";
+"use client";
 import "./page.module.css";
-import TagList from "@/components/tagList";
 import "antd/dist/reset.css";
-import { Layout, Row, Col } from "antd";
+import { Layout, Row, Col, Input, Tag, Tooltip } from "antd";
 import GlobalFeed from "@/components/Feed/GlobalFeed";
 import { Content } from "antd/es/layout/layout";
+import { getTags } from "@/actions/handleTags";
+import { SearchOutlined } from "@ant-design/icons";
+import { useState, useEffect, ChangeEvent } from "react";
 
+export default function Home() {
+    return (
+        <Layout
+            className="col-12 col-md-10 col-xl-8"
+            style={{ minHeight: "100vh", margin: "0 auto" }}
+        >
+            <Content style={{ padding: "0 24px", marginTop: "16px" }}>
+                <Row gutter={24} align="top" className="gap-4">
+                    <Col xs={24} md={16} lg={16} style={{ padding: "0" }}>
+                        <GlobalFeed />
+                    </Col>
 
-export default async function Home() {
-  return (
-    <Layout
-      className="col-12 col-md-10 col-xl-8"
-      style={{ minHeight: "100vh", margin: "0 auto" }}
-    >
-      <Content style={{ padding: "0 24px", marginTop: "16px" }}>
-        <Row gutter={24} align="top" className="gap-4">
-          <Col xs={24} md={16} lg={16} style={{ padding: "0" }}>
-            <GlobalFeed />
-          </Col>
-
-          <Col
-            xs={24}
-            md={6}
-            lg={6}
-            style={{
-              padding: "0",
-              // position: "-webkit-sticky",
-              position: "sticky",
-              top: "80px",
-            }}
-          >
-            <TagList />
-          </Col>
-        </Row>
-      </Content>
-    </Layout>
-  );
+                    <Col
+                        xs={24}
+                        md={6}
+                        lg={6}
+                        style={{
+                            padding: "0",
+                            // position: "-webkit-sticky",
+                            position: "sticky",
+                            top: "80px",
+                        }}
+                    >
+                        <TagList />
+                    </Col>
+                </Row>
+            </Content>
+        </Layout>
+    );
 }
+
+const TagList: React.FC = () => {
+    const [searchValue, setSearchValue] = useState<string>("");
+    const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+    const [tags, setTags] = useState<string[]>();
+
+    useEffect(() => {
+        (async function () {
+            setTags(await getTags());
+        })();
+    }, []);
+
+    const handleTagChange = (value: string) => {
+        if (selectedTags.includes(value)) {
+            setSelectedTags(selectedTags.filter((tag) => tag !== value));
+        } else {
+            setSelectedTags([...selectedTags, value]);
+        }
+    };
+
+    const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.target.value);
+    };
+
+    const handleTagClick = (tag: string) => {
+        setSelectedTags((prev) => {
+            if (prev.includes(tag)) {
+                return prev.filter((t) => t !== tag);
+            } else {
+                return [...prev, tag];
+            }
+        });
+    };
+
+    const filteredTags = tags?.filter((tag) => tag.includes(searchValue));
+
+    return (
+        <div>
+            <Input
+                placeholder="Search tag"
+                prefix={<SearchOutlined />}
+                value={searchValue}
+                onChange={handleSearch}
+            />
+            <div style={{ marginTop: 16 }}>
+                {filteredTags?.map((tag) => (
+                    <Tooltip title={tag} key={tag}>
+                        <Tag
+                            color={
+                                selectedTags.includes(tag) ? "blue" : "default"
+                            }
+                            onClick={() => handleTagClick(tag)}
+                            style={{ cursor: "pointer", marginBottom: 8 }}
+                        >
+                            {tag}
+                        </Tag>
+                    </Tooltip>
+                ))}
+            </div>
+        </div>
+    );
+};
