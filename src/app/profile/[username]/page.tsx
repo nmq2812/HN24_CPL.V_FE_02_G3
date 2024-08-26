116f13c398e97154b6ce43bd95eeec04ca909788
21020718 Nguyễn Thị Thu Trang <21020718@vnu.edu.vn>
2024-08-26 21:41:28 +0700

    Update profile page (load bài viết, UI thêm bài viết, follow user). Cần hoàn thiện follow người dùng, thêm bài viết, sửa UI

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,24 +1,29 @@
-"use client";
+// "use client";
+import { Avatar, Button, Card, Flex, Layout, List, Typography } from "antd";
+import Feed from "@/components/Feed/Feed";
+import { Content } from "antd/es/layout/layout";
+import { cookies } from "next/headers";
+import Link from "antd/es/typography/Link";
+import { getCurrentUser } from "@/actions/authAction";
 import { getProfile } from "@/actions/handleProfile";
-import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
-import { List } from "antd";
 
-export default function ProfilePage({
+export default async function ProfilePage({
     params,
 }: {
     params: { username: string };
 }) {
     const username = params.username;
-    const [profile, setProfile] = useState<Profile>();
+    const res = await getProfile(username);
+    const profile = res.profile;
 
-    useEffect(() => {
-        (async function () {
-            const profile = await (await getProfile(username)).profile;
-            setProfile(profile);
-        })();
-    }, []);
+    const cookieStore = cookies();
+    const token = cookieStore.get("token")?.value ?? "";
+    const searchParams = {
+        author: profile?.username,
+    };
 
+    const userResponse = await getCurrentUser(token);
+    const user = userResponse.success ? userResponse.data : null;
     return (
         <div className="profile-page">
             <div className="user-info">
@@ -34,23 +39,53 @@ export default function ProfilePage({
                                     />
                                     <h4>{profile?.username}</h4>
                                     <p>{profile?.bio}</p>
-                                    <a
-                                        className="btn btn-sm btn-outline-secondary action-btn"
-                                        href="/settings"
-                                    >
-                                        <i className="ion-gear-a" /> Edit
-                                        Profile Settings
-                                    </a>
+                                    {
+                                        username === user?.username ? (
+                                            <a
+                                                className="btn btn-sm btn-outline-secondary action-btn"
+                                                href="/settings"
+                                            >
+                                                <i className="ion-gear-a" /> Edit Profile Settings
+                                            </a>
+                                        ) : (
+                                            <button
+                                                className="btn btn-sm btn-outline-primary"
+                                            // onClick={() => {handleFollowing }}
+                                            >
+                                                Follow
+                                            </button>
+                                        )
+                                    }
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <Layout
+                className="col-12 col-md-10 col-xl-8"
+                style={{ minHeight: "100%", margin: "0 auto" }}
+            >
+                {username === user?.username && (
+                    <Card hoverable style={{ margin: "0 24px" }}>
+                        <Avatar src={profile?.image} />
+                        <Link style={{ margin: '30px', flexGrow: 1, }}
+                            href={`/profile/${profile?.username}`} //sửa link tạo bài viết mới
+                            className="username"
+                        >
+                            What&#39;s on your mind, {profile?.username.split(' ')[0]}?
+                        </Link>
+                    </Card>
+                )}
 
-            <div className="container">
-                <List></List>
-            </div>
+                <Content style={{ padding: "0 24px", marginTop: "16px" }}>
+                    <Feed
+                        fetchUrl="/articles"
+                        optionals={searchParams}
+                        token={token}
+                    />
+                </Content>
+            </Layout>
         </div>
     );
 }
