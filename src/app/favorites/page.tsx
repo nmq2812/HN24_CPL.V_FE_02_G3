3aa65109cf813ffb3e04ecb8d206e198cc93e192
21020383-NGUYENMINHQUAN <nguyenminhquan341@gmail.com>
2024-08-28 23:26:23 +0700

    .

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -2,46 +2,47 @@ import Feed from "@/components/Feed/Feed";
 import { Col, Layout, Row } from "antd";
 import { Content } from "antd/es/layout/layout";
 import { cookies } from "next/headers";
-import TagList from "@/components/Tag/TagList";
+import TagList from "@/components/Tag/tagList";
 import { getCurrentUser } from "@/actions/authAction";
 import { getTags } from "@/actions/handleTags";
+import { CommentType } from "@/types/enums";
 
 async function FavoritesPage({
-  searchParams,
+    searchParams,
 }: {
-  searchParams?: { [key: string]: string | string[] | undefined };
+    searchParams?: { [key: string]: string | string[] | undefined };
 }) {
-  const cookieStore = cookies();
-  const token = cookieStore.get("token")?.value;
-  const user = token && (await getCurrentUser(token)).data;
-  const tags = await getTags();
+    const cookieStore = cookies();
+    const token = cookieStore.get("token")?.value;
+    const user = token && (await getCurrentUser(token)).data;
+    const tags = await getTags();
 
-  searchParams = {
-    ...searchParams,
-    favorited: user.username,
-  };
-  return (
-    <Layout
-      className="col-12 col-lg-10 col-xl-8"
-      style={{ minHeight: "100%", margin: "0 auto" }}
-    >
-      <Content style={{ padding: "0 24px", marginTop: "16px" }}>
-        <Row gutter={24} align="top" className="gap-4">
-          <Col xs={24} lg={14} style={{ padding: "0" }}>
-            <Feed
-              fetchUrl="/articles"
-              optionals={searchParams}
-              token={token}
-              currentUser={user}
-            />
-          </Col>
-          <Col className="tags order-first order-lg-0" xs={24} lg={8}>
-            <TagList tags={tags} />
-          </Col>
-        </Row>
-      </Content>
-    </Layout>
-  );
+    searchParams = {
+        ...searchParams,
+        favorited: user.username,
+    };
+    return (
+        <Layout
+            className="col-12 col-lg-10 col-xl-8"
+            style={{ minHeight: "100%", margin: "0 auto" }}
+        >
+            <Content style={{ padding: "0 24px", marginTop: "16px" }}>
+                <Row gutter={24} align="top" className="gap-4">
+                    <Col xs={24} lg={14} style={{ padding: "0" }}>
+                        <Feed
+                            fetchUrl="/articles"
+                            optionals={searchParams}
+                            token={token}
+                            commentType={CommentType.FeedComment}
+                        />
+                    </Col>
+                    <Col className="tags order-first order-lg-0" xs={24} lg={8}>
+                        <TagList tags={tags} />
+                    </Col>
+                </Row>
+            </Content>
+        </Layout>
+    );
 }
 
 export default FavoritesPage;
