3c067100912885076d4bfe983cd2aef997fabdb7
21020383-NGUYENMINHQUAN <nguyenminhquan341@gmail.com>
2024-10-02 01:36:49 +0700

    .

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,20 +2,20 @@ import FormLogin from "@/components/Form/FormLogin";
 import Link from "next/link";
 
 export default function Login() {
-  return (
-    <div className="auth-page">
-      <div className="container page">
-        <div className="row">
-          <div className="col-md-6 offset-md-3 col-xs-12 text-bg-light border pt-4">
-            <h1 className="text-xs-center">Sign in</h1>
-            <p className="text-xs-center">
-              <Link href="/signup" >Don't have an account ?</Link>
-            </p>
+    return (
+        <div className="auth-page">
+            <div className="container page">
+                <div className="row">
+                    <div className="col-md-6 offset-md-3 col-xs-12 text-bg-light border pt-4 mt-3">
+                        <h1 className="text-center">Sign in</h1>
+                        <p className="text-center">
+                            <Link href="/signup">Don't have an account ?</Link>
+                        </p>
 
-            <FormLogin />
-          </div>
+                        <FormLogin />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,20 +2,20 @@ import FormSignup from "@/components/Form/FormSignup";
 import Link from "next/link";
 
 export default function Signup() {
-  return (
-    <div className="auth-page">
-      <div className="container page">
-        <div className="row">
-          <div className="col-md-6 offset-md-3 col-xs-12 text-bg-light border pt-4">
-            <h1 className="text-xs-center">Sign up</h1>
-            <p className="text-xs-center">
-              <Link href="/login">Have an account?</Link>
-            </p>
+    return (
+        <div className="auth-page">
+            <div className="container page">
+                <div className="row">
+                    <div className="col-md-6 offset-md-3 col-xs-12 text-bg-light border pt-4 mt-3">
+                        <h1 className="text-center">Sign up</h1>
+                        <p className="text-center">
+                            <Link href="/login">Have an account?</Link>
+                        </p>
 
-            <FormSignup />
-          </div>
+                        <FormSignup />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
